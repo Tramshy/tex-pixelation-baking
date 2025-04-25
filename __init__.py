d3f563d6c4dfb4069553b5732e3dcdce93dca692
Tramshy <simon.hansen1000@gmail.com>
2025-04-25 16:29:13 +0200

    Pixelation and Baking Code

---
+++ __init__.py
@@ -0,0 +1,124 @@
+bl_info = {
+    "name": "Pixelation Baking",
+    "author": "Trashy aka Simon",
+    "version": (1, 0),
+    "blender": (4, 1, 0),
+    "location": "View3D > N",
+    "description": "Pixelates and bakes base color of selected objects",
+    "warning": "",
+    "doc_url": "",
+    "category": "UV"
+}
+
+import bpy
+import asyncio
+from bpy.types import (Panel, Operator)
+from bpy.props import StringProperty, IntProperty, BoolProperty, PointerProperty
+from . import apply_pixelation_bake
+# REMOVE
+import importlib
+
+importlib.reload(apply_pixelation_bake)
+
+class ButtonOperator(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "pixelation.1"
+    bl_label = "Pixelation Operator"
+
+    def execute(self, context):
+        properties = context.scene.pixelation_bake_settings
+        
+        # Define parameters
+        models_to_process = bpy.context.selected_objects
+        pixelation_node_group_name = "Pixelation"
+        output_dir = properties.directory_path
+        p_res = properties.pixel_resolution
+        t_res = properties.texture_resolution
+        batches = properties.batches
+        
+        # Run the function
+        asyncio.run(apply_pixelation_bake.bake_in_batches(models_to_process, pixelation_node_group_name, output_dir, p_res, t_res, batches))
+        
+        return {'FINISHED'}
+
+class PixelationPanel(bpy.types.Panel):
+    bl_label = "Pixelation Baking Panel"
+    bl_idname = "OBJECT_PT_pixelation"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Tool"
+
+    def draw(self, context):
+        layout = self.layout
+        obj = context.object
+        
+        properties = context.scene.pixelation_bake_settings
+        
+        layout.prop(properties, "directory_path")
+        layout.label(text="Settings")
+        
+        layout.prop(properties, "pixel_resolution")
+        layout.prop(properties, "texture_resolution")
+        layout.separator()
+        
+        layout.prop(properties, "should_batch")
+        layout.prop(properties, "batches")
+        layout.separator()
+        
+        row = layout.row()
+        row.operator(ButtonOperator.bl_idname, text="Pixelate and Bake")
+
+class AddonProperties(bpy.types.PropertyGroup):
+    directory_path: StringProperty(
+        name="Texture Path",
+        description="The folder to store the baked pixelated color textures.",
+        subtype='DIR_PATH'
+    )
+
+    pixel_resolution: IntProperty(
+        name="Pixelation Resolution",
+        description="The new visual resolution of the base color.",
+        default=256
+    )
+
+    texture_resolution: IntProperty(
+        name="Texture Resolution",
+        description="The size of the baked texture image.",
+        default=1024
+    )
+
+    batches: IntProperty(
+        name="Batch Size",
+        description="The size of each batch.",
+        default=2
+    )
+
+    should_batch: BoolProperty(
+        name="Enable Batching",
+        description="Batching means the objects will be split into several queues, between each queue a small break will occur from the bnaking. This may help performance in some cases",
+        default=False
+    )
+
+from bpy.utils import register_class, unregister_class
+
+_classes = [
+    ButtonOperator,
+    PixelationPanel,
+    AddonProperties
+]
+
+def register():
+    for cls in _classes:
+        register_class(cls)
+        
+    bpy.types.Scene.pixelation_bake_settings = PointerProperty(type=AddonProperties)
+
+
+def unregister():
+    for cls in _classes:
+        unregister_class(cls)
+        
+    del bpy.types.Scene.pixelation_bake_settings
+
+if __name__ == "__main__":
+    register()
---
+++ apply_pixelation_bake.py
@@ -0,0 +1,108 @@
+import bpy
+import asyncio
+
+async def apply_pixelation_and_bake(models, pixelation_node_name, output_directory, resolution=256, bake_resolution=512):
+    scene = bpy.context.scene
+    bake_settings = scene.render.bake
+    
+    start_render_engine = scene.render.engine
+    start_pass_direct = bake_settings.use_pass_direct
+    start_pass_indirect = bake_settings.use_pass_indirect
+    start_pass_color = bake_settings.use_pass_color
+    start_samples = scene.cycles.samples
+    
+    scene.render.engine = "CYCLES"
+    scene.cycles.bake_type = 'DIFFUSE'
+    
+    bake_settings.use_pass_direct = False
+    bake_settings.use_pass_indirect = False
+    bake_settings.use_pass_color = True
+    
+    scene.cycles.samples = 1
+    
+    for obj in models:
+        if not obj.data.materials or not obj.type == 'MESH':
+            continue
+        
+        if len(obj.data.polygons) <= 0:
+            bpy.data.objects.remove(obj, do_unlink=True)
+            continue
+        
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+        
+        for mat in obj.data.materials:
+            if not mat.use_nodes:
+                mat.use_nodes = True
+            
+            node_tree = mat.node_tree
+            nodes = node_tree.nodes
+            
+            found_tex = False
+            
+            for node in mat.node_tree.nodes:
+                if node.type == 'BSDF_PRINCIPLED':
+                    base_color_input = node.inputs['Base Color']
+                    p_node = node
+                    
+                    if base_color_input.is_linked:
+                        linked_node = base_color_input.links[0].from_node
+                        
+                        if linked_node.type == 'TEX_IMAGE':
+                            tex_image = linked_node
+                            found_tex = True
+                            
+                            if pixelation_node_name not in nodes:
+                                pixel_node = nodes.new(type="ShaderNodeGroup")
+                                pixel_node.node_tree = bpy.data.node_groups[pixelation_node_name]
+                            else:
+                                pixel_node = nodes[pixelation_node_name]
+                                
+                            pixel_node.inputs['Resolution'].default_value = resolution
+                            node_tree.links.new(pixel_node.outputs['Vector'], tex_image.inputs['Vector'])
+                
+            if not found_tex:
+                continue
+            
+            # In materials loop
+            start_metallic = p_node.inputs['Metallic'].default_value
+            p_node.inputs['Metallic'].default_value = 0
+            
+            bake_tex = nodes.new(type="ShaderNodeTexImage")
+            bake_image = bpy.data.images.new(name=f"{obj.name}_Baked", width=bake_resolution, height=bake_resolution)
+            bake_tex.image = bake_image
+            
+            p_node.select = False
+            bake_tex.select = True
+            nodes.active=bake_tex
+            
+            bpy.ops.object.bake(type='DIFFUSE')
+            
+            # Bake done
+            bake_image.filepath_raw = f"{output_directory}/{obj.name}_base_color.png"
+            bake_image.file_format = 'PNG'
+            bake_image.save()
+            
+            p_node.inputs['Metallic'].default_value = start_metallic
+            bake_tex.image = bpy.data.images.load(f"{output_directory}/{obj.name}_base_color.png")
+            node_tree.links.new(bake_tex.outputs['Color'], p_node.inputs['Base Color'])
+            
+            nodes.remove(pixel_node)
+            nodes.remove(tex_image)
+            
+    scene.render.engine = start_render_engine
+    bake_settings.use_pass_direct = start_pass_direct
+    bake_settings.use_pass_indirect = start_pass_indirect
+    bake_settings.use_pass_color = start_pass_color
+    scene.cycles.samples = start_samples
+
+def split_list(input_list, batch_size):
+    return [input_list[i:i + batch_size] for i in range(0, len(input_list), batch_size)]
+
+async def bake_in_batches(objects, pixelation_node_group_name, output_dir, resolution, bake_resolution, batch_size):
+    batches = split_list(objects, batch_size)
+    
+    for batch in batches:
+        await apply_pixelation_and_bake(batch, pixelation_node_group_name, output_dir, resolution, bake_resolution)
+        
+        await asyncio.sleep(0.3)
